b9997b1e27fcb9c6b3f399f0fc9cd8e9399287ae
Unik <unikbahadur1852@gmail.com>
2026-05-05 12:49:53 +0545

    CICD pipe

--- src/model/model_building.py
+++ src/model/model_building.py
@@ -19,12 +19,7 @@ import pickle
 import dagshub
 
 mlflow.set_tracking_uri('https://dagshub.com/unikbahadur1852/Laptop-Price-Project.mlflow')
-dagshub.init(
-    repo_owner='unikbahadur1852',
-    repo_name='Laptop-Price-Project',
-    mlflow=True,
-    token=os.getenv("LAPTOP_TEST")
-)
+dagshub.init(repo_owner='unikbahadur1852', repo_name='Laptop-Price-Project', mlflow=True)
 
 
 
